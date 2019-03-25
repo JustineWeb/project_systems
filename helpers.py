5ea2acf45d2b8f41d9afc39c72ec65a19c41d295
Alexis Mermet <alexis.mermet@epfl.ch>
2019-03-25 18:34:22 +0100

    modified helpers

--- helpers.py
+++ helpers.py
@@ -25,20 +25,50 @@ def calculate_accuracy(y, X, w):
     predicted_y = prediction(X, w)
     return accuracy(predicted_y, y)
 
-def calculate_stochastic_gradient(y, X, w, lambda_, n, num_examples):
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def calculate_stochastic_gradient(y, X, w, lambda_,batch_size, num_examples):
     """compute the stochastic gradient of loss plus regularizer.
     X: the dataset matrix, shape = (num_examples, num_features)
     y: the corresponding +1 or -1 labels, shape = (num_examples)
     w: shape = (num_features)
-    n: the index of the (one) datapoint we have sampled
     num_examples: N
     """
-   
-    def is_support(y_n, x_n, w):
-        """a datapoint is support if max{} is not 0. """
-        return y_n * x_n @ w < 1
+	def is_support(y_n, x_n, w):
+		"""a datapoint is support if max{} is not 0. """
+		return y_n * x_n @ w < 1
+	
+	gradient = 0
+
+	for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+		for x,y in tx_batch, y_batch:
+			grad = - y * x.T if is_support(y, x, w) else np.zeros_like(x.T)
+			grad = num_examples * np.squeeze(grad) + lambda_ * w
+			gradient += grad
     
-    x_n, y_n = X[n], y[n]
-    grad = - y_n * x_n.T if is_support(y_n, x_n, w) else np.zeros_like(x_n.T)
-    grad = num_examples * np.squeeze(grad) + lambda_ * w
-    return grad
+    return gradient/batch_size
+   
