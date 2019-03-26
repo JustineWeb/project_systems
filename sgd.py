d30c7665855afaa879a4a7df94a3b5defd59184a
Justine Weber <justine.weber@epfl.ch>
2019-03-26 08:55:59 +0100

    Spark Context & Session created.

--- sgd.py
+++ sgd.py
@@ -2,11 +2,12 @@
 
 import sys
 
-#from pyspark.sql import SparkSession
-
+from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 
 import numpy as np
+
+import helpers
 
 #logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
 #spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
@@ -24,16 +25,26 @@ import numpy as np
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
-        print("Usage: PythonSparkSGD <master> <data_path> <num_iterations> <num_workers>", file=sys.stderr)
+        print("Usage: sgd <data_path> <num_iterations> <num_workers>", file=sys.stderr)
         sys.exit(-1)
 
-    sc = SparkContext(sys.argv[1], "PySparkSGD")
+    # Get arguments
+    data_path = sys.argv[1]
+    n_iterations = int(sys.argv[3])
+    n_workers = int(sys.argv[4])
 
-    path = sys.argv[1]
-    iterations = sys.argv[3]
-    workers = sys.argv[4]
+    # Create Spark environment
+    spark = SparkSession.builder.appName("PySparkSGD").getOrCreate()
+
+    #sc = SparkContext(sys.argv[1], "PySparkSGD")
+    sc = spark.SparkContext
 
     #val rdd = ... ?
-    rdd = sc.textFile(path)
+    rdd = sc.textFile(data_path)
+    # TODO : preprocess file.
+    data = None
+
+    
+
 
     
