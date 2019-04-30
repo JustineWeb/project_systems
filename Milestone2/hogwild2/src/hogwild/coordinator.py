627add0af37658e49e312d9efe9e07064a81e67b
Alexis Mermet <alexis.mermet@epfl.ch>
2019-04-30 09:38:58 +0200

    code finished

--- Milestone2/hogwild2/src/hogwild/coordinator.py
+++ Milestone2/hogwild2/src/hogwild/coordinator.py
@@ -5,10 +5,9 @@ from multiprocessing.sharedctypes import Array
 from datetime import datetime
 import settings as s
 from EarlyStopping import EarlyStopping
-from utils import calculate_accs
 from time import time
 import ingest_data
-from utils import dotproduct, sign
+from utils import dotproduct, sign, calculate_accs
 
 def calculate_svm_update(w, data_train,targets_train):
     # Select random subset
@@ -95,8 +94,6 @@ if __name__ == '__main__':
 		w = Array('d',[0.0]*dim,lock=True)
 
 
-	#pool = multiprocessing.Pool(processes= s.N_WORKERS, initializer=init_worker, initargs=(w,) )
-
 	# Early stopping
 	early_stopping = EarlyStopping(s.persistence)
 	stopping_crit_reached = False
@@ -105,10 +102,10 @@ if __name__ == '__main__':
 	n_epochs = 0
 	start_time = time()
 	losses_val = []
+
 	while n_epochs < s.epochs:
 		workers = [multiprocessing.Process(target=calculate_svm_update, args = (w,data_train,targets_train)) for i in range(s.N_WORKERS)]
 		# update weights vector
-		#pool.map(calculate_svm_update,(w,))
 		for p in workers:
 			p.start()
 		for p in workers:
@@ -129,6 +126,8 @@ if __name__ == '__main__':
 
 		end_time = time()
 	print('All SGD epochs done!')
+	print("final time is: {}".format(end_time))
+	print("time per iteration: {}".format(end_time/n_epochs))
 
     ### Calculating final accuracies on train, validation and test sets ###
 	data_test, targets_test = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
@@ -144,127 +143,61 @@ if __name__ == '__main__':
 	b = sum([1 for x in targets_test if x == 1])
 	print('Val accuracy of Label 1: {:.2f}%'.format(a / b))
 
-'''
-	svm = SVM(learning_rate=s.learning_rate, lambda_reg=s.lambda_reg, dim=dim,w=w)
+	# load the train dataset
+	print('Loading train and validation sets to calculate final accuracies')
+	data, targets = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
+																s.TOPICS_FILE,
+																s.TEST_FILES,
+																selected_cat='CCAT',
+																train=True)
+	data_train, targets_train, data_val, targets_val = ingest_data.train_val_split(data, targets, val_indices)
+	# Calculate the predictions on the train set
+	preds_train = predict(data_train,w)
+	acc_pos_train, acc_neg_train, acc_tot_train = calculate_accs(targets_train, preds_train)
+	print('Train accuracy of Label 1: {:.2f}%'.format(acc_pos_train))
+	print('Train accuracy of Label -1: {:.2f}%'.format(acc_neg_train))
+	print('Train accuracy: {:.2f}%'.format(acc_tot_train))
 
-	# Step 3: depending on the chosen mode, create the pool of workers and the shared array
-	pool = multiprocessing.Pool(processes= s.N_WORKERS)
+	# Calculate the predictions on the validation set
+	pred_vals = predict(data_val,w)
+	acc_pos_val, acc_neg_val, acc_tot_val = calculate_accs(targets_val, preds_val)
+	print('Val accuracy of Label 1: {:.2f}%'.format(acc_pos_val))
+	print('Val accuracy of Label -1: {:.2f}%'.format(acc_neg_val))
+	print('Val accuracy: {:.2f}%'.format(acc_tot_val))
 
-	# Early stopping
-	early_stopping = EarlyStopping(s.persistence)
-	stopping_crit_reached = False
-
-	# Step 4: training using the pool of threads
-	n_epochs = 0
-	start_time = time()
-	losses_val = []
-	while n_epochs < s.epochs:
-
-		# update weights vector
-		pool.map(calculate_svm_update,(data_train,targets_train,svm,w))
-		n_epochs += 1
-
-		# calculate validation loss
-		val_loss = svm.loss(data_val,targets_val)
-		losses_val.append({'time': datetime.utcfromtimestamp(time()).strftime("%Y-%m-%d %H:%M:%S.%f"),'loss_val': val_loss})
-		print('Val loss: {:.4f}'.format(val_loss))
-
-		# check for early stopping
-		stopping_crit_reached = early_stopping.stopping_criterion(val_loss)
-		if stopping_crit_reached:
-			break
-
-		end_time = time()
-		print('All SGD epochs done!')
-
-        ### Calculating final accuracies on train, validation and test sets ###
-		data_test, targets_test = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
+	# Load the test dataset
+	print('Loading test set')
+	data_test, targets_test = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
 																	s.TOPICS_FILE,
 																	s.TEST_FILES,
 																	selected_cat='CCAT',
 																	train=False)
 
-		# Calculate the predictions on the validation set
-		prediction = svm.predict(data_test)
+	# Calculate the predictions on the test set
+	preds_test = predict(data_test, w)
+	acc_pos_test, acc_neg_test, acc_tot_test = calculate_accs(targets_test, preds_test)
+	print('Test accuracy of Label 1: {:.2f}%'.format(acc_pos_test))
+	print('Test accuracy of Label -1: {:.2f}%'.format(acc_neg_test))
+	print('Test accuracy: {:.2f}%'.format(acc_tot_test))
 
-		a = sum([1 for x in zip(targets_test, prediction) if x[0] == 1 and x[1] == 1])
-		b = sum([1 for x in targets_test if x == 1])
-		print('Val accuracy of Label 1: {:.2f}%'.format(a / b))
+	# Save results in a log
+	log = [{'start_time': datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S.%f"),
+			'end_time': datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S.%f"),
+			'running_time': end_time - start_time,
+			'n_workers': s.N_WORKERS,
+			'running_mode': s.running_mode,
+			'sync_epochs': n_epochs - 1,
+			'accuracy_train': acc_tot_train,
+			'accuracy_1_train': acc_pos_train,
+			'accuracy_-1_train': acc_neg_train,
+			'accuracy_val': acc_tot_val,
+			'accuracy_1_val': acc_pos_val,
+			'accuracy_-1_val': acc_neg_val,
+			'accuracy_test': acc_tot_test,
+			'accuracy_1_test': acc_pos_test,
+			'accuracy_-1_test': acc_neg_test,
+			'losses_val': losses_val,
+			'losses_train': hws.train_losses}]
 
-		# Load the train dataset
-		print('Loading train and validation sets to calculate final accuracies')
-		data, targets = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
-		                                                    s.TOPICS_FILE,
-		                                                    s.TEST_FILES,
-		                                                    selected_cat='CCAT',
-		                                                    train=True)
-		data_train, targets_train, data_val, targets_val = ingest_data.train_val_split(data, targets, val_indices)
-
-        # Calculate the predictions on the train set
-        task_queue.put({'type': 'predict', 'values': data_train})
-        preds_train = response_queue.get()
-        acc_pos_train, acc_neg_train, acc_tot_train = calculate_accs(targets_train, preds_train)
-        print('Train accuracy of Label 1: {:.2f}%'.format(acc_pos_train))
-        print('Train accuracy of Label -1: {:.2f}%'.format(acc_neg_train))
-        print('Train accuracy: {:.2f}%'.format(acc_tot_train))
-
-        # Calculate the predictions on the validation set
-        task_queue.put({'type': 'predict', 'values': data_val})
-        preds_val = response_queue.get()
-        acc_pos_val, acc_neg_val, acc_tot_val = calculate_accs(targets_val, preds_val)
-        print('Val accuracy of Label 1: {:.2f}%'.format(acc_pos_val))
-        print('Val accuracy of Label -1: {:.2f}%'.format(acc_neg_val))
-        print('Val accuracy: {:.2f}%'.format(acc_tot_val))
-
-        # Load the test dataset
-        print('Loading test set')
-        data_test, targets_test = ingest_data.load_large_reuters_data(s.TRAIN_FILE,
-                                                                      s.TOPICS_FILE,
-                                                                      s.TEST_FILES,
-                                                                      selected_cat='CCAT',
-                                                                      train=False)
-
-        # Calculate the predictions on the test set
-        task_queue.put({'type': 'predict', 'values': data_test})
-        preds_test = response_queue.get()
-        acc_pos_test, acc_neg_test, acc_tot_test = calculate_accs(targets_test, preds_test)
-        print('Test accuracy of Label 1: {:.2f}%'.format(acc_pos_test))
-        print('Test accuracy of Label -1: {:.2f}%'.format(acc_neg_test))
-        print('Test accuracy: {:.2f}%'.format(acc_tot_test))
-
-        # Save results in a log
-        log = [{'start_time': datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S.%f"),
-                'end_time': datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S.%f"),
-                'running_time': end_time - start_time,
-                'n_workers': s.N_WORKERS,
-                'running_mode': s.running_mode,
-                'sync_epochs': n_epochs - 1,
-                'accuracy_train': acc_tot_train,
-                'accuracy_1_train': acc_pos_train,
-                'accuracy_-1_train': acc_neg_train,
-                'accuracy_val': acc_tot_val,
-                'accuracy_1_val': acc_pos_val,
-                'accuracy_-1_val': acc_neg_val,
-                'accuracy_test': acc_tot_test,
-                'accuracy_1_test': acc_pos_test,
-                'accuracy_-1_test': acc_neg_test,
-                'losses_val': losses_val,
-                'losses_train': hws.train_losses}]
-
-        with open('log.json', 'w') as outfile:
-            json.dump(log, outfile)
-
-        # Send poison pill to SVM process
-        task_queue.put(None)
-
-        # Close queues and join processes
-        task_queue.close()
-        task_queue.join_thread()
-        response_queue.close()
-        response_queue.join_thread()
-        svm_proc.join()
-
-    except KeyboardInterrupt:
-        server.stop(0)
-
-'''
+	with open('log.json', 'w') as outfile:
+		json.dump(log, outfile)
